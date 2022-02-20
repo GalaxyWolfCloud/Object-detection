b8337a915c3887f1a1606fa60d06732e077d6b45
GalaxyWolfCloud <79249476+GalaxyWolfCloud@users.noreply.github.com>
2022-02-20 17:15:58 +0530

    Add files via upload

--- TV.js
+++ TV.js
@@ -27,4 +27,24 @@ function gotResults(error,results){
 
 function draw(){
     image(TV_image,0,0,640,350);
+    if( status != ""){
+        for (i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence *100);
+            text(objects[i].label+""+percent+"%", objects[i].x+ 15, objects[i].y+ 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+        }
+    fill("#FF0000");
+    text("TV", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect( 30, 60, 450, 350 );
+    }
+    
+    function back(){
+        window.location = "index.html";
 }
--- fan.js
+++ fan.js
@@ -1,5 +1,6 @@
 Status = "";
 fan_image = "";
+objects = []
 
 function preload(){
     fan_image = loadImage("fan.jpg");
@@ -27,4 +28,27 @@ function gotResults(error,results){
 
 function draw(){
     image(fan_image,0,0,640,350);
-}
+    if( status != ""){
+        for (i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence *100);
+            text(objects[i].label+""+percent+"%", objects[i].x+ 15, objects[i].y+ 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+        }
+    fill("#FF0000");
+    text("fan", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect( 30, 60, 450, 350 );
+    }
+    
+    function back(){
+        window.location = "index.html";
+    }
+
+
+
